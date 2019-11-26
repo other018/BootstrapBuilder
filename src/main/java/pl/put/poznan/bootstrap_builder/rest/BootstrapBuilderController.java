a5e2a1cb3a6fd79ea0181c31bc71ce89c0fe7115
Marcel Lipczynski <marcellipczynski@gmail.com>
2019-11-26 18:13:20 +0100

    Changed package name

---
+++ src/main/java/pl/put/poznan/bootstrap_builder/rest/BootstrapBuilderController.java
@@ -0,0 +1,10 @@
+package pl.put.poznan.bootstrap_builder.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/html")
+public class BootstrapBuilderController {
+
+
+
+}
