bf130f55ee19354672644b40161f24b566443759
Marcel Lipczynski <marcellipczynski@gmail.com>
2019-11-30 12:27:30 +0100

    Changed RequestParam to PathVariable

--- src/main/java/pl/put/poznan/bootstrap_builder/rest/BootstrapBuilderController.java
+++ src/main/java/pl/put/poznan/bootstrap_builder/rest/BootstrapBuilderController.java
@@ -2,10 +2,7 @@ package pl.put.poznan.bootstrap_builder.rest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.put.poznan.bootstrap_builder.logic.*;
 
 import java.util.ArrayList;
@@ -34,8 +31,8 @@ public class BootstrapBuilderController {
 //        return htmlSkeletons;
 //    }
 
-    @GetMapping(produces = "application/json")
-    public String getElementByDescription(@RequestParam("description") String description){
+    @GetMapping(produces = "application/json", value = "/{description}")
+    public String getElementByDescription(@PathVariable String description){
         Optional<HTML> searchedHTML = htmlSkeletons.stream().
                 filter(html -> html.getDescription().equals(description)).findFirst();
         logger.debug("Requested for HTML object with: " + description + " description");
